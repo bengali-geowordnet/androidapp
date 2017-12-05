8efdfcd902c95aa9668538662df8f793b5bfd0d2
Farhan Ar Rafi <farhan.rafi@bjitgroup.com>
2017-12-05 16:30:44 +0600

    GEO_ANDROID: Minor change for user URL
    - Minor change for user data URL
    
    Refs - N/A

--- app/src/main/java/com/farhanarrafi/geonames/bngeonames/fragments/UserInfoFragment.kt
+++ app/src/main/java/com/farhanarrafi/geonames/bngeonames/fragments/UserInfoFragment.kt
@@ -24,7 +24,7 @@ class UserInfoFragment : Fragment(), ResponseCallback {
         if (arguments != null) {
         }
         url = Preferences.get(context, Constants.SERVER_LIST,
-                Constants.DEFAULT_SERVER_URL) + Constants.DATA_URL
+                Constants.DEFAULT_SERVER_URL) + Constants.USER_URL
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
